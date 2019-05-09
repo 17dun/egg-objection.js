73e244e1dbfe3471c6463dda90d13ae4b0468700
arden <arden@tujiao.com>
2019-05-09 13:24:46 +0800

    add async/await transaction for knex

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ function initObjection(config, app) {
   const objection = require('objection');
   const Knex = require('knex');
   const knex = Knex(config.knex);
+
+  promisifyTransaction(knex);
+
+  app.knex = knex;
+
   objection.Model.knex(knex);
 
   const modelDir = path.join(app.baseDir, 'app', config.baseDir);
@@ -47,4 +52,54 @@ function initObjection(config, app) {
   Object.assign(app[delegate], app[target]);
 
   return objection;
+}
+
+
+function promisifyTransaction (client) {
+    const proto = Reflect.getPrototypeOf(client.client);
+
+    if (proto._promisify_transaction) {
+        return;
+    }
+
+    proto._promisify_transaction = true;
+
+    proto._raw_transaction = proto.transaction;
+
+    proto.transaction = function (...args) {
+        if (typeof args[0] === 'function') {
+            if (isGenerator(args[0])) args[0] = co.wrap(args[0]);
+            return proto._raw_transaction.apply(this, args);
+        }
+        let config;
+        let outTx;
+        if (args.length > 0) outTx = args.pop();
+        if (args.length > 0) config = args.pop();
+
+        return new Promise(resolve => {
+            const transaction = proto._raw_transaction.apply(this, [
+                function _container (trx) {
+                    resolve(trx);
+                },
+                config,
+                outTx,
+            ]);
+
+            transaction.rollback = function (conn, error) {
+                return this.query(conn, 'ROLLBACK', error ? 2 : 1, error)
+                    .timeout(5000)
+                    .catch(Promise.TimeoutError, () => {
+                        this._resolver();
+                    });
+            };
+
+            transaction.rollbackTo = function (conn, error) {
+                return this.query(conn, `ROLLBACK TO SAVEPOINT ${this.txid}`, error ? 2 : 1, error)
+                    .timeout(5000)
+                    .catch(Promise.TimeoutError, () => {
+                        this._resolver();
+                    });
+            };
+        });
+    };
 }
